85131b6f2451f1e24bc89a06bbbeeb534ff3bc73
1998AshwiniDBhagat <ashbhagat19122@gmail.com>
2022-11-11 21:58:03 +0530

    FIR-2:crate required package structure

--- FirstGitJiraApp/src/main/java/com/BikkadIT/FirstGitJiraAppApplication.java
+++ FirstGitJiraApp/src/main/java/com/BikkadIT/FirstGitJiraAppApplication.java
@@ -8,6 +8,8 @@ public class FirstGitJiraAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstGitJiraAppApplication.class, args);
+		
+		System.out.println("Working file");
 	}
 
 }
